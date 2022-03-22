6c17c304f840c409557a49d77e3b9669bad43b3a
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2022-03-22 18:30:55 -0400

    teste

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PageOne from "./page-one";
+import PageTwo from "./page-two";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/one" element={<PageOne />} />
+        <Route path="/two" element={<PageTwo />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
