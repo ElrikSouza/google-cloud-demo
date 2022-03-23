23513d3f450b5efe7a6cdbc232ed26250851c991
ElrikSouza <72892051+ElrikSouza@users.noreply.github.com>
2022-03-23 14:03:51 -0400

    base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://storage.googleapis.com/cb-frontend/",
-  server: {
-    origin: "https://storage.googleapis.com/cb-frontend",
-  },
+  base: "/cb-frontend/",
 });
